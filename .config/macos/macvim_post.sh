8f8edc71fa61e1ec8e447e7fe067f2e30cc3f431
Jinwook Jeong <vustthat@gmail.com>
2021-09-04 23:27:15 +0900

    fix macvim config

--- .config/macos/macvim_post.sh
+++ .config/macos/macvim_post.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-APP_PATH=
-if [ -e /Applications/MacVim.app ]; then
-  >&2 echo 'nothing to do'
+APP_PATH=/Applications/MacVim.app
+if ! [ -e "$APP_PATH" ]; then
+  >&2 echo 'Please install MacVim.app'
   exit 1
 fi
 
-BREW_PATH=$(brew --prefix macvim 2>/dev/null)
-BREW_PATH=${BREW_PATH:+$BREW_PATH/MacVim.app}
-if [ ! "$BREW_PATH" ]; then
-  >&2 echo "MacVin is not installed"
-  exit 1
-fi
+# MacVim executables
+cd && mkdir -p .local/bin
+cd "$HOME/.local/bin" && \
+  ln -fs "$APP_PATH/Contents/bin/m"* ./
 
-osascript -e 'tell application "Finder" to make alias file to POSIX file "'${BREW_PATH}'" at POSIX file "/Applications"'
-APP_PATH=$BREW_PATH
-
+# MacVim manpages
 cd && mkdir -p .local/share/man/man1
 cd "$HOME/.local/share/man/man1" && \
   ln -fs "$APP_PATH/Contents/Resources/vim/runtime/doc/"*.1 ./
